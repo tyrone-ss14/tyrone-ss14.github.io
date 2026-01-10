87b4efad90012ff69f73ace5ceb427a45e112a31
tyrone-ss14 <aaaaa.blyat.aaaaa@gmail.com>
2026-01-10 11:09:07 +0100

    Add live update feature for editor input

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const editor = document.getElementById("editor");
+const paper = document.getElementById("paper");
 
+function update() {
+  paper.innerHTML = parseSS14(editor.value);
+}
+
+editor.addEventListener("input", update);
+update();
