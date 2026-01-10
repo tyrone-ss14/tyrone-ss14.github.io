527ec6be0e2ceebb78172dfd608b2c957b47eddf
tyrone-ss14 <aaaaa.blyat.aaaaa@gmail.com>
2026-01-10 11:23:10 +0100

    Add parseSS14 function for text parsing

--- parser.js
+++ parser.js
@@ -1,1 +1,15 @@
+function parseSS14(text) {
+  let output = "";
+  let i = 0;
 
+  while (i < text.length) {
+
+    // tag handling here...
+
+    // plain text fallback
+    output += text[i];
+    i++;
+  }
+
+  return output;
+}
