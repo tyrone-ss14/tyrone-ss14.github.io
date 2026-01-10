6837f33c7af51545eda2c7baf7ea7b87663e317f
tyrone-ss14 <aaaaa.blyat.aaaaa@gmail.com>
2026-01-10 11:48:26 +0100

    Log update event in the console

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const editor = document.getElementById("editor");
 const paper = document.getElementById("paper");
 
 function update() {
+  console.log("update fired", editor.value);
   paper.innerHTML = parseSS14(editor.value);
 }
 
