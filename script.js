8d7506620cb1af4b359be3ee6a63b9103c26cb29
tyrone-ss14 <aaaaa.blyat.aaaaa@gmail.com>
2026-01-10 05:59:04 +0100

    Add text rendering functionality to script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const input = document.getElementById("input");
+const paper = document.getElementById("paper");
+
+function render() {
+  let text = input.value;
+
+  text = text.replace(/\[head=1\](.*?)\[\/head\]/gis, "<h1>$1</h1>");
+  text = text.replace(/\[bold\](.*?)\[\/bold\]/gis, "<b>$1</b>");
+  text = text.replace(/\n/g, "<br>");
+
+  paper.innerHTML = text;
+}
+
+input.addEventListener("input", render);
+render();
